35fdbc7a09e2b23a5f7e694187285cb6f8862eb7
NicklasGV <nvedeby@gmail.com>
2023-03-23 08:42:10 +0100

    23-03-2023

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceId: 'e14jydn8v5id',
+    token: 'w5LBjru9apBjB59lGYody26nEnMOJy3Txjme0oIctNQ'
+  }
 };
